1f9aea15b9905c56d95c42f7b427b2bf94d3c1be
Abhishek Dubey <AbhishekAshokDubey@users.noreply.github.com>
2017-04-19 12:16:48 +0530

    Update mask_data.py

--- mask_data.py
+++ mask_data.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  8 00:31:39 2017
+
+@author: ADubey4
+"""
+
 import pandas as pd
 import numpy as np
 import numbers
@@ -6,13 +13,19 @@ from string import ascii_uppercase
 import pickle
 from math import ceil
 
-not_to_touch_column = []
+import os
 
-noise_type = 'uniform' # 'uniform', 'normal', 'laplace'
+os.chdir(r"C:\Users\Adubey4\Desktop\microsoft\problem-statements-ms\WL");
+
+not_to_touch_column = ["field_ticket", "start_date", "end_date", "job_days"]
+
+file_name = "wl_eq_rp_train_ftl"
+
+noise_type = 'normal' # 'uniform', 'normal', 'laplace'
 noise_var_1 = 0 # mean or min
 noise_var_2 = None # std or max, make None for scaled values
 noise_percentage_of_std = 1; # if noise_var_2 is None
-data_file_path = r"some_file_path\some_file_name.csv";
+data_file_path = r"path\\" + file_name + ".csv";
 outlier_percentage = 1;
 outlier_std_range = [3,5]; # noise_var_2 for the outlier distribution
 
@@ -23,6 +36,8 @@ def gen_rand(noise_type="normal",val_1 = 0, val_2 = 1, size = 1):
     if (np.isnan(val_1) or np.isnan(val_2)):
         val_1 = 0;
         val_2 = 1;
+    if(val_1>=0 and val_2==0): # constant std case
+        val_2 = 0.001;
     if noise_type == "uniform":
         noise = np.random.uniform(low=val_1, high=val_2, size=size)
     elif noise_type == "normal":
@@ -60,15 +75,21 @@ def replace_with_dict(df_passed, masking_dict):
 def make_dependent_column(df_passed, new_col_name= "col"):
     df = df_passed.copy();
     column_names = df.columns;
-    no_of_columns = np.random.randint(1,len(column_names),1)
-    random_indexes = np.random.choice(range(len(column_names)),no_of_columns, replace=False)
-    df_consider = df.ix[:,random_indexes]
-    for i,col_name in enumerate(df_consider):
-        if i==0:continue;
-        df_consider.ix[:,0] = df_consider.ix[:,0] + df_consider[col_name]
-    df_return = df_consider.ix[:,0]
-    df_return.name = new_col_name
-    return df_return, df_consider.columns
+    if len(column_names) == 0:
+        return None, None
+    elif len(column_names) == 1:
+        df_return = np.random.randint(1,10) * df.ix[:,0] +  np.random.randint(1,ceil(np.mean(df.ix[:,0])))
+        return df_return, ""
+    else:
+        no_of_columns = np.random.randint(1,len(column_names),1)
+        random_indexes = np.random.choice(range(len(column_names)),no_of_columns, replace=False)
+        df_consider = df.ix[:,random_indexes]
+        for i,col_name in enumerate(df_consider):
+            if i==0:continue;
+            df_consider.ix[:,0] = df_consider.ix[:,0] + df_consider[col_name]
+        df_return = df_consider.ix[:,0]
+        df_return.name = new_col_name
+        return df_return, df_consider.columns
 
 # TODO: chnage from pass by reference to pass by variable
 #       Also add values on the negative sides
@@ -80,8 +101,6 @@ def add_outliers(df_passed, noise_type="uniform"):
         random_indexes = np.random.choice(range(row_count),outlier_count, replace=False);
         mean_temp = np.mean(df[col_name]);
         std_temp = np.std(df[col_name]);
-        print("--------------------")
-        print(col_name)
         outliers = gen_rand(noise_type,
                             mean_temp + (outlier_std_range[0]*std_temp),
                             mean_temp + (outlier_std_range[1]*std_temp),
@@ -97,12 +116,18 @@ def add_outliers(df_passed, noise_type="uniform"):
 
 ################# user code #################
 ###############################################################################
-df =  pd.read_csv(data_file_path)
+df =  pd.read_csv(data_file_path,encoding="ISO-8859-1")
 col_names = df.columns.drop(not_to_touch_column)
 data_points_count = len(df)
 
 num_cols = df._get_numeric_data().columns
 char_cols = df.columns.drop(num_cols)
+
+to_drop_numeric_columns = [x for x in num_cols if x in not_to_touch_column];
+num_cols = num_cols.drop(to_drop_numeric_columns)
+
+to_drop_char_columns = [x for x in char_cols if x in not_to_touch_column];
+char_cols = char_cols.drop(to_drop_char_columns)
 
 df[num_cols] = add_outliers(df[num_cols])
 
@@ -121,8 +146,9 @@ for col_name in char_cols:
     df[col_name] = df_char[col_name];
 
 df_dep, merged_columns = make_dependent_column(df[num_cols], "new_col")
-df = pd.concat([df,df_dep],axis=1)
+if df_dep is not None:
+    df = pd.concat([df,df_dep],axis=1)
 
-pickle.dump( masking_dict, open( "cleaning_raw_masking_dict.p", "wb" ) )
+pickle.dump( masking_dict, open( r"path\\" + file_name +"_masking_dict.p", "wb" ) )
 # masking_dict = pickle.load( open( "masking_dict.p", "rb" ) )
-df.to_csv(r"file_path\masked_file_name.csv",index=False);
+df.to_csv(r"path\\"+ file_name +"_masked.csv",index=False);
